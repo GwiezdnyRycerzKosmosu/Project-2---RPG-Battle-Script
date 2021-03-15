2f23dbe15f9858f610ed25a84f9f1ff0b9e5553f
GwiezdnyRycerzKosmosu <m.kaminski.gik@gmail.com>
2021-03-15 21:08:06 +0100

    magic improved as separated class

--- classes/game.py
+++ classes/game.py
@@ -1,5 +1,6 @@
 import random
-
+from .Magic import Spell
+import pprint
 
 class bcolors:
     HEADER = '\033[95m'
@@ -26,14 +27,6 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def generate_spell_damage(self):
-        return random.randrange(self.atkl,self.atkh)
-
-    def generate_spell_damage(self, i):
-        mgl = self.magic[i]["dmg"]-5
-        mgh = self.magic[i]["dmg"]+5
-        return random.randrange(mgl,mgh)
-
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
@@ -55,11 +48,6 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
 
     def choose_action(self):
         i = 1
@@ -70,8 +58,9 @@ class Person:
 
     def choose_magic(self):
         i = 1
+
         print(bcolors.OKBLUE+bcolors.BOLD+"Magic"+bcolors.ENDC)
         for spell in self.magic:
-            print(str(i)+":", str(spell["name"]), "(cost:", str(spell["cost"]) + ")" )
+            print(str(i)+":", spell.name, "(cost:", str(spell.cost) + ")" )
             i += 1
 
